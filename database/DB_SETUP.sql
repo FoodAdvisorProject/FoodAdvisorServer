851d8fd977e8ae73af933fc0c19452e455b3e830
Gabrio94 <gabriot1994@gmail.com>
2017-02-01 16:12:54 +0100

    Update DB_SETUP.sql

--- database/DB_SETUP.sql
+++ database/DB_SETUP.sql
@@ -5,12 +5,12 @@ CREATE TABLE UTENTE (
 	email               varchar(255) not null unique,
 	nome                char(25) not null, cognome char(25) not null,
 	is_azienda          bool not null,
-  descrizione_azienda varchar(255), # a check that forces not null if is_azienda is missing
-  foto                blob         # allows null for debugging purpose
+    descrizione_azienda varchar(255), # a check that forces not null if is_azienda is missing
+    foto                blob         # allows null for debugging purpose
 );
 
 CREATE TABLE ARTICOLO (
-	  id          bigint       NOT NULL PRIMARY KEY AUTO_INCREMENT ,
+	id          bigint       NOT NULL PRIMARY KEY AUTO_INCREMENT ,
     nome        varchar(255) NOT NULL UNIQUE,
     id_creatore bigint       NOT NULL REFERENCES UTENTE(id),
     descrizione varchar(255) NOT NULL,
@@ -19,7 +19,7 @@ CREATE TABLE ARTICOLO (
 );
 
 CREATE TABLE TRANSAZIONE (
-	  id            bigint NOT NULL PRIMARY KEY AUTO_INCREMENT ,
+    id            bigint NOT NULL PRIMARY KEY AUTO_INCREMENT ,
     id_articolo   bigint NOT NULL REFERENCES ARTICOLO(id),
     id_compratore bigint NOT NULL REFERENCES UTENTE(id),
     id_venditore  bigint NOT NULL REFERENCES UTENTE(id),
