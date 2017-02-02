bc466dab1d9c249b2877772a0d00deb8e019b8c0
Gabrio94 <gabriot1994@gmail.com>
2017-02-02 15:34:30 +0100

    let's speak english

--- database/DB_SETUP.sql
+++ database/DB_SETUP.sql
@@ -1,35 +1,40 @@
+SET foreign_key_checks=0;
+DROP TABLE IF EXISTS ARTICOLO,TRANSAZIONE,UTENTE; 
+SET foreign_key_checks=1;
+
 CREATE TABLE UTENTE (
-	id                  bigint NOT NULL PRIMARY KEY AUTO_INCREMENT, 
-	login               varchar(25) NOT NULL UNIQUE,
-	passw               varchar(255) not null,
-	email               varchar(255) not null UNIQUE,
-	nome                char(25) not null, 
-	cognome             char(25) not null,
-	is_azienda          bool not null,
-	descrizione_azienda varchar(255), # a check that forces not null if is_azienda is missing
-	foto                blob          # allows null for debugging purpose
+	id                     bigint       NOT NULL PRIMARY KEY AUTO_INCREMENT, 
+	login                  varchar(25)  NOT NULL UNIQUE,
+	passw                  varchar(255) NOT NULL,
+	email                  varchar(255) NOT NULL UNIQUE,
+	name                   char(25)     NOT NULL , 
+	second_name            char(25)     NOT NULL ,
+	is_enterprise          bool         NOT NULL ,
+	enterprise_description varchar(255), # a check that forces not null if is_azienda is missing
+	photo                  blob          # allows null for debugging purpose
 );
 
 CREATE TABLE ARTICOLO (
-	id          bigint       NOT NULL PRIMARY KEY AUTO_INCREMENT ,
-	nome        varchar(255) NOT NULL ,
-	id_creatore bigint       NOT NULL ,
-	descrizione varchar(255) NOT NULL,
-	foto        blob,
-	FOREIGN KEY (id_creatore) REFERENCES UTENTE(id),
-	UNIQUE(nome,id_creatore)
+	id           bigint       NOT NULL PRIMARY KEY AUTO_INCREMENT ,
+	name         varchar(255) NOT NULL ,
+	id_creator   bigint       NOT NULL ,
+	description  varchar(255) NOT NULL,
+	photo        blob,
+	FOREIGN KEY (id_creator) REFERENCES UTENTE(id),
+	UNIQUE(name,id_creator)
 );
 
 #@FIX a check that forces that if id_venditore is null than id_compratore=id_creatore is missing
 CREATE TABLE TRANSAZIONE (
 	id            bigint NOT NULL PRIMARY KEY AUTO_INCREMENT ,
-	id_articolo   bigint NOT NULL ,
-	id_compratore bigint NOT NULL ,
-	id_venditore  bigint          ,#NULL value identifies that id_compratore == id_creatore
-	longitudine   float(10,6)     ,#allows null for debugging purpose
-	latitudine    float(10,6)     ,
-	FOREIGN KEY (id_articolo)   REFERENCES ARTICOLO(id),
-	FOREIGN KEY (id_compratore) REFERENCES UTENTE(id),
-	FOREIGN KEY (id_venditore ) REFERENCES UTENTE(id),
-	UNIQUE(id_articolo,id_compratore,id_venditore)
+	id_article    bigint NOT NULL ,
+	id_buyer      bigint NOT NULL ,
+	id_seller     bigint          ,#NULL value identifies that id_buyer == id_creator
+	longitude     float(10,6)     ,#allows null for debugging purpose
+	latitude      float(10,6)     ,
+	FOREIGN KEY   (id_article)  REFERENCES ARTICOLO(id),
+	FOREIGN KEY   (id_buyer)    REFERENCES UTENTE(id),
+	FOREIGN KEY   (id_seller )  REFERENCES UTENTE(id),
+	UNIQUE(id_article,id_buyer,id_seller)
 );
+
